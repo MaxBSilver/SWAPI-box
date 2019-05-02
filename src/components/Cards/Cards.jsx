95a571768b7ca800bda22d31b9e7328ccdf5f746
Max Silver <Maxbsilver@gmail.com>
2019-05-02 08:36:10 -0600

    nav styling

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,8 +3,11 @@ import Card from "../Card/Card";
 
 export default function Cards(props) {
   console.log(props);
+  const formatCategory = (categoryString) => {
+    return categoryString.toUpperCase()
+  }
   return (<React.Fragment>
-    <h1>{props.category}</h1>
+    <h1 className="category-text"><div>{formatCategory(props.category)}</div></h1>
     <section className="cards">
       
       {props.display.map(item => {
